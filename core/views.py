7f0d79be94d94201b61fda3a5cdc3ce81b8e2ffb
Pablo <pablo.mikolaiczyki@catolicasc.edu.br>
2024-04-04 23:49:41 -0300

    Adicionada funcionalidade para botão de interação newslatter

--- core/views.py
+++ core/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
-def index(request):
+def str(request):
     return render(request, "index.html")
+
+
+def index(request):
+    useremail = None
+    username = None
+    userfone = None
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        useremail = request.POST['useremail']
+        userfone = request.POST['userfone']
+
+    return render(request, 'index.html', {'username': username, 'useremail': useremail, 'userfone': userfone})
--- projeto_apoiase/urls.py
+++ projeto_apoiase/urls.py
@@ -21,5 +21,5 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("home", views.index, name="index"),
+    path("", views.index),
 ]
